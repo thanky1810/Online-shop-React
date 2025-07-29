f5d2babf638af482d865c8b39d26908b6f60cd7c
thanky1810 <thanky4444@gmail.com>
2025-07-29 20:48:38 +0700

    home page

---
+++ src/components/banner.jsx
@@ -0,0 +1,11 @@
+import nikeJustDoIt from "../assets/images/nike-just-do-it.png";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img src={nikeJustDoIt} />
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "../components";
+
+function Home() {
+  return (
+    <div className="experince-wrapper">
+      <Banner></Banner>
+    </div>
+  );
+}
+
+export default Home;
