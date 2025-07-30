3ee25268b7e2e43b53fda4f15cbc3849b2faf678
thanky1810 <thanky4444@gmail.com>
2025-07-30 11:48:44 +0700

    Banner

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -5,8 +5,22 @@ import nikeImage from "../../assets/images/nike-just-do-it.png";
 
 function Banner() {
   return (
-    <div>
+    <div className={clsx(styles.banner)}>
       <img src={nikeImage} className={clsx(styles.mainImg)} />
+      <figcaption className={clsx(styles.wrapper)}>
+        <div className={clsx(styles.contentWrapper)}>
+          <p className={clsx(styles.p1)}>Look of Football</p>
+          <h3 className={clsx(styles.h3)}>FIELD GENERAL</h3>
+          <p className={clsx(styles.p2)}>
+            Crafted with purpose. Driven by passion. Designed for everyday
+          </p>
+          <div>
+            <a href="#" className={styles.button}>
+              Shop
+            </a>
+          </div>
+        </div>
+      </figcaption>
     </div>
   );
 }
