083e325a9f0ce71206a3334ae18fd5b9c4dd2833
thanky1810 <thanky4444@gmail.com>
2025-08-08 13:29:14 +0700

    Banner

---
+++ src/pages/Men/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import clsx from "clsx";
+import styles from "./banner.module.scss";
+import { useState, useEffect } from "react";
+
+function Banner() {
+
+
+  return <div className={clsx(styles.wrapper)}></div>;
+}
+
+export default Banner;
--- src/pages/Men/men.jsx
+++ src/pages/Men/men.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components";
 import HeaderMen from "./HeaderMen/HeaderMen";
+import Banner from "./Banner/Banner";
 
 function Men() {
   return (
     <div>
       <Header></Header>
       <HeaderMen></HeaderMen>
+      <Banner></Banner>
     </div>
   );
 }
